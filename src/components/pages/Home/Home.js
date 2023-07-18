3f5724d1caaf2404111ae22c7eeecb107a7e6e1d
GARCIADANIELYS <danielysgarcia@outlook.it>
2023-07-18 13:33:35 +0200

    trackApiResponse funciona, img y albums renderizados en pantalla

--- src/components/pages/Home/Home.js
+++ src/components/pages/Home/Home.js
@@ -1,13 +1,38 @@
-import spotifyBackground from '../../../assets/spotify-background.png';
 import './Home.css';
+import { useContext } from 'react';
+import { ApiContext } from '../../../services/Api';
 
 const Home = () => {
+  const { trackApiResponse } = useContext(ApiContext);
+  //  console.log(trackApiResponse);
 
-  return (
-    <div className='home-page'>
-    <img src={spotifyBackground} alt='spotify background'/>
-    </div>
-  )
+  if (trackApiResponse && trackApiResponse.tracks && trackApiResponse.tracks.items) {
+    return (
+      <div className='home'>
+        <h1 className='home-title'>BIENVENIDO A LA HOME</h1>
+
+        <div className='tracks-container'>
+          {trackApiResponse.tracks.items.map((track) => (
+            <div className='track-album-div' key={track.id}>
+            
+              {track.name && <p>{track.name}</p>}
+              
+              {track.album && track.album.images && track.album.images.length > 0 && (
+                <img className='track-album-image' src={track.album.images[0].url} alt={track.album.name} />
+              )}
+
+              {track.artists && track.artists.length > 0 && (
+                <p>{track.artists[0].name}</p>
+              )}
+            </div>
+          ))}
+        </div>
+
+      </div>
+    );
+  }
+
+  return null;
 }
 
 export default Home;
