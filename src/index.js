df89d3a37b004af9f745caccb0f055910b48cdbc
GARCIADANIELYS <danielysgarcia@outlook.it>
2023-07-18 10:14:48 +0200

    llamada API tracks funcionando

--- src/index.js
+++ src/index.js
@@ -11,7 +11,6 @@ root.render(
     <ApiContextProvider>
       <App />
     </ApiContextProvider>
-  
 );
 
 // If you want to start measuring performance in your app, pass a function
--- src/services/Api.js
+++ src/services/Api.js
@@ -5,23 +5,23 @@ ApiContext.displayName = "ApiContext"
 
 
 export const ApiContextProvider = ({ children }) => {
-  
-  const [apiResponse, setApiResponse] = useState([]);
-  const [search, setSearch] = useState("house"); 
-  const [type, setType] = useState("artist") //se puede cambiar el type y buscar por album", "artist", "playlist", "track", "show", "episode", "audiobook"
- 
+
+  const [ apiResponse, setApiResponse ] = useState([]);
+  const [ search, setSearch ] = useState("house");
+  const [ type, setType ] = useState("artist") //se puede cambiar el type y buscar por album", "artist", "playlist", "track", "show", "episode", "audiobook"
+
   const search_URL = `https://api.spotify.com/v1/search?q=${search}&type=${type}`
   const profile_URL = `https://api.spotify.com/v1/me`
 
-  const [endpoint, setEndpoint] = useState("")
- 
+  const [ endpoint, setEndpoint ] = useState("")
+
   useEffect(() => {
     setEndpoint(search_URL); //se puede atualizar que endpoint llamar
-  }, [search_URL]);
-  
-   
+  }, [ search_URL ]);
+
+
   useEffect(() => {
-    const fetchData = async () => {
+    const fetchTrackData = async () => {
       try {
         let authParams = {
           method: "GET",
@@ -41,14 +41,64 @@ export const ApiContextProvider = ({ children }) => {
       }
     }
 
+    fetchTrackData();
+  }, [ endpoint ]);
+
+
+  // petición API para obtener canciones para la Home
+  const [ trackApiResponse, setTrackApiResponse ] = useState([]);
+
+  const track_URL = "https://api.spotify.com/v1/search?q=album&type=track&limit=20"; // URL con q= album | type=track | limit=20 tracks
+
+  const [ trackEndpoint, setTrackEndpoint ] = useState("");
+
+  useEffect(() => {
+    setTrackEndpoint(track_URL);
+  }, [ track_URL ]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        let authParams = {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: "Bearer " + window.localStorage.access_token,
+          },
+        };
+
+        const res = await fetch(trackEndpoint, authParams);
+        const data = await res.json();
+
+        setTrackApiResponse(data);
+        console.log("Respuesta de la API para los tracks", data);
+      } catch (error) {
+        console.log("intento de API call fallido")
+      }
+    }
+
     fetchData();
-  }, [endpoint]);
- 
+  }, [ trackEndpoint ]);
 
 
   return (
-    <ApiContext.Provider value={{ apiResponse, search, setSearch, type, setType, endpoint, setEndpoint,search_URL, profile_URL }}>
-    {children}
+    <ApiContext.Provider
+      value={{
+        apiResponse,
+        search,
+        setSearch,
+        type,
+        setType,
+        endpoint,
+        setEndpoint,
+        search_URL,
+        profile_URL,
+        track_URL,
+        trackApiResponse,
+        trackEndpoint,
+        setTrackEndpoint
+      }}>
+      {children}
     </ApiContext.Provider>
   );
 };
