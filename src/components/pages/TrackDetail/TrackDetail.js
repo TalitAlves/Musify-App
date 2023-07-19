a4be6e80bb09a79ff4b2a8d949016b8dd9f58670
GARCIADANIELYS <danielysgarcia@outlook.it>
2023-07-19 08:20:52 +0200

    track details funciona

--- src/components/pages/TrackDetail/TrackDetail.js
+++ src/components/pages/TrackDetail/TrackDetail.js
@@ -5,10 +5,10 @@ const TrackDetail = () => {
     //con useParams me traigo los datos del Id del track
     const { trackId } = useParams();
     const [ trackDetails, setTrackDetails ] = useState(null);
+    console.log(trackDetails);
 
     useEffect(() => {
         const fetchData = async () => {
-            console.log("prueba fetchData ()");
             try {
                 const response = await fetch(`https://api.spotify.com/v1/tracks/${trackId}`, {
                     headers: {
@@ -38,10 +38,12 @@ const TrackDetail = () => {
 
     return (
         <div>
-            <h1>AQUÍ TIENES LOS DETALLES DEL TRACK SELECCIONADO</h1>
+            <h1>ESTOS SON LOS DETALLES DEL TRACK</h1>
             <h3>{trackDetails.artists[0].name}</h3>
-            <p>Album: {trackDetails.album.album_type}</p>
-            <p>Artista: {trackDetails.artists[0].type}</p>
+            <p>Tipo de álbum: {trackDetails.album.album_type}</p>
+            <p>Nombre del álbum: {trackDetails.album.name}</p>
+            <p>Tipo: {trackDetails.artists[0].type}</p>
+            <p>Mercados en los que está disponible: {trackDetails.album.available_markets}</p>
         </div>
     )
 }
