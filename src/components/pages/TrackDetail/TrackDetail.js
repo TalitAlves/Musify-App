1a6dac67339a437e2b2ff6bf0ea5c9742d380f7d
GARCIADANIELYS <danielysgarcia@outlook.it>
2023-07-19 19:00:22 +0200

    track details

--- src/components/pages/TrackDetail/TrackDetail.js
+++ src/components/pages/TrackDetail/TrackDetail.js
@@ -1,11 +1,11 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router';
+import './TrackDetails.css';
 
 const TrackDetail = () => {
     //con useParams me traigo los datos del Id del track
     const { trackId } = useParams();
     const [ trackDetails, setTrackDetails ] = useState(null);
-    console.log(trackDetails);
 
     useEffect(() => {
         const fetchData = async () => {
@@ -19,7 +19,7 @@ const TrackDetail = () => {
                 const data = await response.json();
 
                 setTrackDetails(data);
-                console.log("respuesta api en trackDetails",data);
+                console.log("respuesta fetch trackId", data);
             } catch (error) {
                 console.log("Error al obtener los detalles del track:", error);
             }
@@ -30,20 +30,28 @@ const TrackDetail = () => {
 
     if (!trackDetails) {
         return (
-        <div>
-        <p>cargando detalles</p>
-        </div>
+            <div>
+                <p>cargando detalles</p>
+            </div>
         )
     }
 
     return (
-        <div>
-            <h1>ESTOS SON LOS DETALLES DEL TRACK</h1>
-            <h3>{trackDetails.artists[0].name}</h3>
-            <p>Tipo de álbum: {trackDetails.album.album_type}</p>
+        <div className='track-details-container'>
+            <h1 className='track-details-title'>DETALLES DEL TRACK</h1>
+            <h1 className='track-details-track'>{trackDetails.name}</h1>
+            <img 
+            src={trackDetails.album.images[ 0 ].url} 
+            alt={trackDetails.album.name}
+            className='track-details-img'
+             />
+            <h3>Nombre de la canción: {trackDetails.artists[ 0 ].name}</h3>
+            <p>Popularidad de la canción según nuestros usuarios: {trackDetails.popularity}</p>
+            <p>Tipo: {trackDetails.album.album_type} {trackDetails.type}</p>
             <p>Nombre del álbum: {trackDetails.album.name}</p>
-            <p>Tipo: {trackDetails.artists[0].type}</p>
-            <p>Mercados en los que está disponible: {trackDetails.album.available_markets}</p>
+            <p>Tipo: {trackDetails.artists[ 0 ].type}</p>
+            <p>duración en milisegundos: {trackDetails.duration_ms} ms</p>
+            <p>Mercados en los que está disponible actualmente: {trackDetails.album.available_markets.join(", ")}</p>
         </div>
     )
 }
