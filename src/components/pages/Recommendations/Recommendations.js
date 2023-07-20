c482b5a8a8d355de8fee896fc1548dc638f0a9b8
GARCIADANIELYS <danielysgarcia@outlook.it>
2023-07-20 17:15:23 +0200

    rec css

--- src/components/pages/Recommendations/Recommendations.js
+++ src/components/pages/Recommendations/Recommendations.js
@@ -2,6 +2,7 @@ import React from 'react'
 import './Recommendations.css';
 import { useContext } from 'react';
 import { ApiContext } from '../../../services/Api';
+import { BeatLoader } from 'react-spinners';
 
 const Recommendations = () => {
   const { recApiResponse } = useContext(ApiContext);
@@ -10,7 +11,7 @@ const Recommendations = () => {
   //console.log("TRACKS", tracks);
 
   if (!recApiResponse) {
-    return <div>Loading...</div>;
+    return <BeatLoader color="#36D7B7" loading={true} size={30} />;
   }
 
   if (recApiResponse && recApiResponse.tracks && recApiResponse.seeds) {
@@ -19,18 +20,15 @@ const Recommendations = () => {
         <h1 className='recommendations-title'>
           Specially For You</h1>
 
-        <div className='rec-tracks-container'>
-          <div className='rec-tracks-card'>
+          <div className='rec-tracks-container'>
             {tracks.map((track) => (
-              <div key={track.id}>
-                <h3>{track.name}</h3>
-                <img src={track.album.images[ 0 ].url} alt={track.name} />
-                <h4>nombre artista</h4>
+              <div className='rec-track-card' key={track.id}>
+                <h3 className='rec-trackname'>{track.name}</h3>
+                <img className='rec-track-img' src={track?.album.images[ 0 ].url} alt={track.name} />
+                <h4>{track?.artists[0]?.name}</h4>
               </div>
             ))}
           </div>
-
-        </div>
 
       </div>
     )
